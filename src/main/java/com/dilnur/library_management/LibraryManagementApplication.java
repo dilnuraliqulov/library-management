effd68a907d9d945778bdfb1f52e13b7116be621
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2026-06-10 22:12:47 +0500

    DataSourceAutoConfiguration class excluded

--- src/main/java/com/dilnur/library_management/LibraryManagementApplication.java
+++ src/main/java/com/dilnur/library_management/LibraryManagementApplication.java
@@ -2,8 +2,10 @@ package com.dilnur.library_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class LibraryManagementApplication {
 
 	public static void main(String[] args) {

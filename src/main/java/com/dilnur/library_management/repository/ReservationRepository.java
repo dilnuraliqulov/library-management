aadb715389d6ac357fb23cd0d8a6b9542a5217fe
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2026-06-13 17:19:41 +0500

    FineRepository,LoadRepository and ReservationRepository added

---
+++ src/main/java/com/dilnur/library_management/repository/ReservationRepository.java
@@ -0,0 +1,20 @@
+package com.dilnur.library_management.repository;
+
+import com.dilnur.library_management.entity.Book;
+import com.dilnur.library_management.entity.Enum.ReservationStatus;
+import com.dilnur.library_management.entity.Member;
+import com.dilnur.library_management.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, UUID>  {
+
+    List<Reservation> findByBookAndStatusOrderByReservationAtAsc(UUID bookId, ReservationStatus status);
+
+    Optional<Reservation> findByMemberAndBookAndStatus(Member member, Book book, ReservationStatus status);
+}

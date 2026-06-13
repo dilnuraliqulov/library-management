801334130d9519e0adb1a562e65433e5cd952514
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2026-06-13 16:22:26 +0500

    AuthorRepository repo added.Added missing @Repository annotation in MemberRepository

--- src/main/java/com/dilnur/library_management/repository/MemberRepository.java
+++ src/main/java/com/dilnur/library_management/repository/MemberRepository.java
@@ -3,10 +3,12 @@ package com.dilnur.library_management.repository;
 import com.dilnur.library_management.entity.Enum.MemberStatus;
 import com.dilnur.library_management.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 
     Optional<Member> findByIdAndStatus(UUID id, MemberStatus status);

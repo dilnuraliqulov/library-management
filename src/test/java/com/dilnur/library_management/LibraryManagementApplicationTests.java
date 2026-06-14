d55e7df39203aa79848fc4898c3fd75ade7f3251
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2026-06-14 17:46:47 +0500

    Fixed bug

--- src/test/java/com/dilnur/library_management/LibraryManagementApplicationTests.java
+++ src/test/java/com/dilnur/library_management/LibraryManagementApplicationTests.java
@@ -2,8 +2,12 @@ package com.dilnur.library_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class LibraryManagementApplicationTests {
 
 	@Test
